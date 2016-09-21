fa25c30eb1be2eaeb153d0b52ff72fb4577c9517
Artur Ashyrov <artur.ashyrov@gmail.com>
2016-09-21 20:18:45 +0300

    Initial commit

---
+++ src/main/java/se/tain/CasinoApiFacadeApplication.java
@@ -0,0 +1,12 @@
+package se.tain;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CasinoApiFacadeApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CasinoApiFacadeApplication.class, args);
+    }
+}
---
+++ src/main/java/se/tain/web/PlayerController.java
@@ -0,0 +1,10 @@
+package se.tain.web;
+
+import org.springframework.stereotype.Controller;
+
+/**
+ * Created by ruel on 9/21/16.
+ */
+@Controller("/player")
+public class PlayerController {
+}
