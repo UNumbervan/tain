bce30fb0d049ef1d373c68b8be31ff815f67c087
Ievhen Lubianov <Ievhen_Lubianov@epam.com>
2017-04-01 16:37:00 +0300

    generate project backbone

--- src/main/resources/static/frontend/src/app/app.module.ts
+++ src/main/resources/static/frontend/src/app/app.module.ts
@@ -4,10 +4,20 @@ import { FormsModule } from '@angular/forms';
 import { HttpModule } from '@angular/http';
 
 import { AppComponent } from './app.component';
+import { GamesComponent } from './games/games.component';
+import { GameComponent } from './games/game/game.component';
+import { CategoriesComponent } from './categories/categories.component';
+import { CategoryComponent } from './categories/category/category.component';
+import { HeaderComponent } from './header/header.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    GamesComponent,
+    GameComponent,
+    CategoriesComponent,
+    CategoryComponent,
+    HeaderComponent
   ],
   imports: [
     BrowserModule,
